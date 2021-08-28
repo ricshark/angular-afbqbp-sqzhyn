b7f13f9aab2f61e5c87d109e22dca09292f211c9
ricshark <ricshark@gmail.com>
2021-08-28 17:58:49 -0300

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -18,10 +18,7 @@ export class ProductDetailsComponent implements OnInit {
     private cartService: CartService
   ) { }
 
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
-    window.alert('Your product has been added to the cart!');
-  }
+  
 
   ngOnInit() {
     // First get the product id from the current route
@@ -30,6 +27,11 @@ export class ProductDetailsComponent implements OnInit {
 
     //Find the product that correspond with the id provided in route.
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 
 }
