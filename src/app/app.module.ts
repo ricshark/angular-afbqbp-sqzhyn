d4fa42ca44f759708ac0f478b8ce9c6265ef0eb1
ricshark <ricshark@gmail.com>
2021-09-09 11:59:24 -0300

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -11,15 +12,18 @@ import { ProductAlertsComponent } from
 import { ProductDetailsComponent } from
 './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
@@ -29,6 +33,7 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductDetailsComponent,    
     CartComponent,
+    ShippingComponent,
   ],
   bootstrap: [
     AppComponent
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({   
@@ -7,6 +7,10 @@ import { Product } from './products';
 })
 export class CartService {
   items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ){}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -19,6 +23,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
-
 }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>
+    ('/assets/shipping.json');
+  }
 }
