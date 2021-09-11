7538c22308a98c34a614e1001abb49ef0f2e5cca
ricshark <ricshark@gmail.com>
2021-09-11 20:15:26 -0300

    1 file modified, 3 added - random button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ProductDetailsComponent } from
 './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { RandomButtonComponent } from './random-button/random-button.component';
 
 @NgModule({
   imports: [
@@ -24,6 +25,7 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent},
       { path: 'shipping', component: ShippingComponent },
+      { path: 'random-button', component: RandomButtonComponent },
     ])
   ],
   declarations: [
@@ -34,6 +36,7 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductDetailsComponent,    
     CartComponent,
     ShippingComponent,
+    RandomButtonComponent,
   ],
   bootstrap: [
     AppComponent
---
+++ src/app/random-button/random-button.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'random-button',
+  template: `
+    <div id="btn" 
+      (click)="onClick()" 
+      (mousedown)="onMousedown()" 
+      (mouseup)="onMouseup()" 
+      [class.clicked]="clicked">
+      {{ random }}
+    </div>  
+  `,
+  styles: [`
+    #btn {
+      width: 100px;
+      height: 100px;
+      margin: 30px auto 0;
+      border-radius: 50%;
+      background: #EF454A;
+      text-align: center;
+      line-height: 105px;
+      color: #FFF;
+      font-size: 42px;
+      font-weight: bold;
+      cursor: pointer;
+      opacity: .9;
+      box-shadow: 0 5px 0 #D1483E;
+      border: 3px solid #D1483E;
+      user-select: none;
+    }
+
+    #btn:hover {
+      opacity: 1;
+    }
+
+    #btn.clicked {
+      margin-top: 36px;
+      box-shadow: 0 4px 0 #D1483E;
+    }  
+  `]
+})
+export class RandomButtonComponent  {
+  random = '?';
+  clicked = false;
+
+  onClick() {
+    // 1 ~ 10의 랜덤 정수
+    this.random = Math.floor((Math.random() * 10) + 1).toString();
+  }
+
+  onMousedown() {
+    this.clicked = true;
+  }
+
+  onMouseup() {
+    this.clicked = false;
+  }
+}
